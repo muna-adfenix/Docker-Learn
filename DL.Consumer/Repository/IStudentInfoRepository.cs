4edf991e74b62ea43c626e9c8c05bd9aa8859f2e
Muna <muna@adfenix.com>
2018-01-25 13:31:33 +0600

    First Commit

---
+++ DL.Consumer/Repository/IStudentInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DL.Consumer.Model;
+
+namespace DL.Consumer.Repository
+{
+    public interface IStudentInfoRepository
+    {
+        Task<List<StudentInfo>> GetStudentInfoAsync();
+
+        Task<StudentInfo> GetStudentInfoAsync(int id);
+
+        Task<StudentInfo> InsertStudentInfo(StudentInfo customer);
+        Task<bool> UpdateStudentInfoAsync(StudentInfo customer);
+        Task<bool> DeleteStudentInfoAsync(int id);
+    }
+}
---
+++ DL.Producer/Controllers/PublicationController.cs
@@ -0,0 +1,38 @@
+﻿using DL.RabbitMQ.Core.Infrastructure;
+using DL.RabbitMQ.Domain.Command;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace DL.Producer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PublicationController : Controller
+    {
+        private readonly IRabbitMQPublisher _rabbitMqPublisher;
+
+        public PublicationController(IRabbitMQPublisher rabbitMqPublisher)
+        {
+            this._rabbitMqPublisher = rabbitMqPublisher;
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            var person = new Student
+            {
+                Name = "Muna",
+                Email = "muna@adfenix.com",
+                Age = 28
+            };
+            this._rabbitMqPublisher.Publish(person, ExchangeType.Direct, "docker.test.exchange", "docker.test.queue");
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Student student)
+        {
+          this._rabbitMqPublisher.Publish(student, ExchangeType.Direct, "docker.test.exchange", "docker.test.queue");
+        }
+  }
+}
